a4d6e138a0416305034fe6664c39c7842d8b2473
Nikolay Krasnov <krasnovkola2004z@gmail.com>
2021-05-07 21:56:00 +0500

    bug fix

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -39,11 +39,11 @@ const Team = () => {
             </a>
           </div>
         </div>
-        <div className={`d-flex flex-column align-items-center text-center mb-5 ${styles.member}`}>
+        <div className='d-flex flex-column align-items-center text-center mb-5'>
           <img className={styles.photo} src={member2} />
           <h5 className='mt-4'>Alexa Biru</h5>
           <p className='text-secondary mb-4'>Office Director</p>
-          <div className={`d-flex justify-content-between ${styles.contactsGroup}`}>
+          <div className={`d-flex justify-content-between w-100 mx-auto ${styles.contactsGroup}`}>
             <a href='https://www.google.ru/' className={`position-relative ${styles.contact}`}>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className={`bi bi-facebook position-absolute translate-middle top-50 start-50 ${styles.contactIcon}`} viewBox="0 0 16 16">
                 <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
@@ -66,11 +66,11 @@ const Team = () => {
             </a>
           </div>
         </div>
-        <div className={`d-flex flex-column align-items-center text-center mb-5 ${styles.member}`}>
+        <div className='d-flex flex-column align-items-center text-center mb-5'>
           <img className={styles.photo} src={member3} />
           <h5 className='mt-4'>Ronober Sing</h5>
           <p className='text-secondary mb-4'>Admin</p>
-          <div className={`d-flex justify-content-between ${styles.contactsGroup}`}>
+          <div className={`d-flex justify-content-between w-100 mx-auto ${styles.contactsGroup}`}>
             <a href='https://www.google.ru/' className={`position-relative ${styles.contact}`}>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className={`bi bi-facebook position-absolute translate-middle top-50 start-50 ${styles.contactIcon}`} viewBox="0 0 16 16">
                 <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
